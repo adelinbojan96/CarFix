ef5945ea0ca4fea5e82034a7165f826ef34aa243
Irimies Vasile <133998309+Vasile07@users.noreply.github.com>
2024-09-13 13:36:25 +0300

    refactor: change cors config

--- BujiiBE/src/main/java/bujii/be/config/CorsConfig.java
+++ BujiiBE/src/main/java/bujii/be/config/CorsConfig.java
@@ -12,8 +12,9 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8081")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowCredentials(true);
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(false);
     }
 }
